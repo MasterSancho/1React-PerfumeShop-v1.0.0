6817c12585a9436f3a9ffbf60536cfe7593ec8a9
Alex Maltsev <57605252+MasterSancho@users.noreply.github.com>
2023-01-27 17:16:12 +0200

    003->Users mail changed

--- server/data/users.js
+++ server/data/users.js
@@ -2,19 +2,19 @@ import bcrypt from 'bcryptjs';
 
 const users = [
  {
-  name: 'Admin User',
-  email: 'admin@example.com',
+  name: 'Admin',
+  email: 'admin@gmail.com',
   password: bcrypt.hashSync('123456', 10),
   isAdmin: true,
  },
  {
   name: 'John Doe',
-  email: 'john@example.com',
+  email: 'john@gmail.com',
   password: bcrypt.hashSync('123456', 10),
  },
  {
   name: 'Jane Doe',
-  email: 'Jane@example.com',
+  email: 'Jane@gmail.com',
   password: bcrypt.hashSync('123456', 10),
  },
 ];
